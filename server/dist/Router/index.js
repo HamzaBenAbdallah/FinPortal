bab1bb043c81bb2ccd3ede26790aa9c0c17fe77f
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-09 19:22:26 -0500

    Update Express to use Typescript

---
+++ server/dist/Router/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+const router = Router();
+router.get("/", (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: "Hello World",
+    });
+});
+export default router;
+//# sourceMappingURL=index.js.map
