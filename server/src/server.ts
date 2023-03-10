b4d29647011a60531d7f2f2cbf17d2f34351aab6
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-10 16:47:14 -0500

    Connect to Mongo and start the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,75 @@
 import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
-dotenv.config();
-
-import router from "./routes/index.js";
-
-const PORT = 4000;
+import http from "http";
+import mongoose from "mongoose";
+import { config } from "./config/config.js";
+import Logging from "./library/Logging.js";
 
 const app = express();
 
-// Middleware
-app.use(morgan("tiny"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Connect to MongoDB
+mongoose
+  .connect(config.mongo.url, { retryWrites: true, w: "majority" })
+  .then(() => {
+    Logging.info("Connected to MongoDB");
+    StartServer();
+  })
+  .catch((err) => Logging.error(err));
 
-// Routes
-app.use("/api", router);
+// Start server if MongoDB is connected
+const StartServer = () => {
+  app.use((req, res, next) => {
+    Logging.info(
+      `Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+    );
 
-app.get("*", (req, res) => {
-  res.status(404).json({
-    status: 404,
-    message: "Page not found",
+    res.on("finish", () => {
+      Logging.info(
+        `Outgoing -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}] - Status: [${res.statusCode}]`
+      );
+    });
+
+    next();
   });
-});
 
-app.listen(PORT);
+  /** Middleware */
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  /** API Rules */
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
+    }
+
+    next();
+  });
+
+  /** Routes */
+
+  /** Health check */
+  app.get("/ping", (req, res, next) =>
+    res.status(200).json({ message: "pong" })
+  );
+
+  /** Error handling */
+  app.use((req, res, next) => {
+    const error = new Error("Not found");
+    Logging.error(error);
+
+    return res.status(404).json({ message: error.message });
+  });
+
+  http.createServer(app).listen(config.server.port, () => {
+    Logging.info(`Server started on port ${config.server.port}`);
+  });
+};
