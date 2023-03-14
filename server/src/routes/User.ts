b7fcccf7fc8e7579bdedab2e47f4a8c4d6376344
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-14 16:13:31 -0400

    Update the user router

--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -1,22 +1,15 @@
-import { Schemas } from "./../middleware/ValidateSchema.js";
 import express from "express";
 import controller from "../controllers/User.js";
-import { ValidateSchema } from "../middleware/ValidateSchema.js";
+import extractFirebaseInfo from "../middleware/extractFirebaseInfo.js";
 
 const router = express.Router();
 
-router.post(
-  "/create",
-  ValidateSchema(Schemas.user.create),
-  controller.createUser
-);
-router.get("/get/:userId", controller.readUser);
-router.get("/get", controller.readAll);
-router.patch(
-  "/update/:userId",
-  ValidateSchema(Schemas.user.update),
-  controller.updateUser
-);
+router.get("/validate", extractFirebaseInfo, controller.validateUser);
+router.get("/:userId", controller.readUser);
+router.post("/create", extractFirebaseInfo, controller.createUser);
+router.post("/login", extractFirebaseInfo, controller.loginUser);
+router.get("/", controller.readAll);
+router.patch("/update/:userId", controller.updateUser);
 router.delete("/delete/:userId", controller.deleteUser);
 
 export default router;
