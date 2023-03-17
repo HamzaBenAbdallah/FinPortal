02f0eec486b5b60e30fbac2a6f4e9216bf11fd5a
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-17 00:34:45 -0400

    Create a log in page

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -1,100 +1,55 @@
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import firebase from "firebase/compat";
-import UserContext from "Context/user";
+import { useLogin } from "./useLogin";
 import IPageProps from "Interfaces/page";
-import {
-    Authenticate,
-    SignInWithSocialMedia as SocialMediaPopup,
-} from "Modules/auth";
-import ErrorText from "Components/ErrorText";
 import { Providers } from "Config/firebase";
-import Loading from "Components/Loading";
+import GoogleButton from "Components/GoogleButton";
+import Separator from "Components/Separator";
+import FormInput from "Components/FormInput";
+import SubmitButton from "Components/SubmitButton";
+import {
+    Wrapper,
+    Container,
+    Title,
+    FormContainer,
+    Prompt,
+    Signup,
+    Reset,
+} from "./Login.styles";
 
 const Login = (props: IPageProps) => {
-    const navigate = useNavigate();
-    const [authenticating, setAuthenticating] = useState(false);
-    const [error, setError] = useState<string>("");
-    const userContext = useContext(UserContext);
-
-    const isLogin = window.location.pathname.includes("login");
-
-    const SignInWithSocialMedia = (provider: firebase.auth.AuthProvider) => {
-        if (error !== "") setError("");
-        setAuthenticating(true);
-
-        SocialMediaPopup(provider)
-            .then(async (result) => {
-                let user = result.user;
-                if (user) {
-                    let uid = user.uid;
-                    let name = user.displayName;
-
-                    if (name) {
-                        try {
-                            let fire_token = await user.getIdToken();
-                            Authenticate(
-                                uid,
-                                name,
-                                fire_token,
-                                (error, _user) => {
-                                    if (error) {
-                                        setError(error);
-                                        setAuthenticating(false);
-                                    } else if (_user) {
-                                        userContext.userDispatch({
-                                            type: "login",
-                                            payload: {
-                                                user: _user,
-                                                fire_token,
-                                            },
-                                        });
-                                        navigate("/");
-                                    }
-                                }
-                            );
-                        } catch (error) {
-                            setError("invalid token");
-                            console.error(error);
-                            setAuthenticating(false);
-                        }
-                    } else {
-                        setError(
-                            "The identity provider did not return a name."
-                        );
-                        setAuthenticating(false);
-                    }
-                } else {
-                    setError(
-                        "The identity provider is missing a lot of the necessary information."
-                    );
-                    setAuthenticating(false);
-                }
-            })
-            .catch((error) => {
-                setError(error.message);
-                setAuthenticating(false);
-            });
-    };
+    const { authenticating, error, SignInWithSocialMedia } = useLogin();
 
     return (
-        <>
-            <div>{isLogin ? "Login" : "Sign Up"}</div>
-            <div>
-                <ErrorText error={error} />
-                <button
+        <Wrapper>
+            <Container>
+                <Title>Welcome Back</Title>
+                <GoogleButton
                     disabled={authenticating}
                     onClick={() => SignInWithSocialMedia(Providers.google)}
-                    style={{
-                        backgroundColor: "#ea4335",
-                        borderColor: "#ea4335",
-                    }}
-                >
-                    Sign {isLogin ? "in" : "up"} with Google
-                </button>
-                {authenticating ? <Loading></Loading> : null}
-            </div>
-        </>
+                />
+                <Separator />
+                <FormContainer>
+                    <FormInput
+                        label="Email"
+                        type="email"
+                        placeholder="your@email.com"
+                        error="Required"
+                    />
+                    <FormInput
+                        label="Password"
+                        type="password"
+                        placeholder="enter your password"
+                        error="Required"
+                    />
+                    <SubmitButton>Sign in</SubmitButton>
+                </FormContainer>
+                <Prompt>
+                    Need an account? <Signup>Sign up</Signup>
+                </Prompt>
+                <Prompt>
+                    Forgot your password? <Reset>Reset it</Reset>
+                </Prompt>
+            </Container>
+        </Wrapper>
     );
 };
 
---
+++ client/src/Pages/Login/useLogin.ts
@@ -0,0 +1,80 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import firebase from "firebase/compat";
+import UserContext from "Context/user";
+import {
+    Authenticate,
+    SignInWithSocialMedia as SocialMediaPopup,
+} from "Modules/auth";
+
+export const useLogin = () => {
+    const navigate = useNavigate();
+    const userContext = useContext(UserContext);
+
+    const [authenticating, setAuthenticating] = useState(false);
+    const [error, setError] = useState<string>("");
+
+    const SignInWithSocialMedia = (provider: firebase.auth.AuthProvider) => {
+        if (error !== "") setError("");
+        setAuthenticating(true);
+
+        SocialMediaPopup(provider)
+            .then(async (result) => {
+                let user = result.user;
+                if (user) {
+                    let uid = user.uid;
+                    let name = user.displayName;
+
+                    if (name) {
+                        try {
+                            let fire_token = await user.getIdToken();
+                            Authenticate(
+                                uid,
+                                name,
+                                fire_token,
+                                (error, _user) => {
+                                    if (error) {
+                                        setError(error);
+                                        setAuthenticating(false);
+                                    } else if (_user) {
+                                        userContext.userDispatch({
+                                            type: "login",
+                                            payload: {
+                                                user: _user,
+                                                fire_token,
+                                            },
+                                        });
+                                        navigate("/");
+                                    }
+                                }
+                            );
+                        } catch (error) {
+                            setError("invalid token");
+                            console.error(error);
+                            setAuthenticating(false);
+                        }
+                    } else {
+                        setError(
+                            "The identity provider did not return a name."
+                        );
+                        setAuthenticating(false);
+                    }
+                } else {
+                    setError(
+                        "The identity provider is missing a lot of the necessary information."
+                    );
+                    setAuthenticating(false);
+                }
+            })
+            .catch((error) => {
+                setError(error.message);
+                setAuthenticating(false);
+            });
+    };
+
+    return {
+        authenticating,
+        error,
+        SignInWithSocialMedia,
+    };
+};
